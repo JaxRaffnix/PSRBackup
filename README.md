6487456343e2aa0649d00b804eeb1bc7223e8ac1
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2025-05-23 21:51:13 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# PSRBackup
+Shorthand PSRB for Powershell Restic Backup.
+
+## TO DO
+
+- Kalender Backup?
+- Kontakte Backup?
+- OneNote Backup
+- Playnite Backup: https://api.playnite.link/docs/manual/library/backup.html
+- NVIDIA Profile Backup: profile inspector
+- Thunderbird Backup
+- File Backup
---
+++ core/Generate-Manifest.ps1
@@ -0,0 +1,10 @@
+New-ModuleManifest -Path .\WinSetup.psd1 `
+    -RootModule 'PSRBackup.psm1' `
+    -ModuleVersion '1.0.0' `
+    -Author 'Jan Hoegen' `
+    -Description 'Simplifies Windows backup process. Utilizes restic.' `
+    -ProjectUri 'https://github.com/JaxRaffnix/WinSetup' `
+    -PowerShellVersion '5.1' `
+    -ScriptsToProcess "core/Initialize-Module.ps1" `
+    -FunctionsToExport  @(
+    )
